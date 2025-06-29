f985a7020398022e488837da03bdff0456f63392
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-06-29 22:10:50 +0200

    comments

--- javascript.js
+++ javascript.js
@@ -13,9 +13,10 @@ const output = document.getElementById("output");
 
 // function for double log
 const log = (message) => {
-    console.log(message); // show in console
-    //
-    const p = document.createElement("p"); // show on page
+    // show in console
+    console.log(message); 
+    // show on page
+    const p = document.createElement("p"); 
     p.textContent = message;
     output.appendChild(p);
 };
